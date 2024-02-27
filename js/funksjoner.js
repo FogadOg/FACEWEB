57a571d6f172bc90e9c6404f80f3af16e76b0525
Fogado Medhane Semena <fog0408>
2024-02-27 09:07:01 +0100

    done

--- js/funksjoner.js
+++ js/funksjoner.js
@@ -8,3 +8,28 @@ function trist()
     document.getElementById('bilde').src="./img/trist.jpg";    
     }
 
+function glad(){
+    document.getElementById('bilde').src="./img/glad.jpg";    
+}
+function redd(){
+    document.getElementById('bilde').src="./img/redd.jpg";    
+}
+
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function tilfeldig(){
+    const rndInt = randomInt(0, 3)
+    const moods=["sur", "trist", "glad", "redd"]
+    
+    document.getElementById('bilde').src=`./img/${moods[rndInt]}.jpg`;   
+}
+
+
+
+
+
+
+   
+    
